ac1d1c66df99f8de84c376b88f9228292e1506ce
Mariana Usuga <marianausuga275004@outlook.es>
2021-07-01 13:01:50 -0500

    click in square to show 'x' with javascript

---
+++ triqui.js
@@ -0,0 +1,25 @@
+
+
+
+$(".square").click(function(){
+    var square=$(this).attr("name")
+    $("."+square).addClass("show")
+})
+$(".square4").click(function(){
+    $(".x4").addClass("show")
+})
+$(".square5").click(function(){
+    $(".x5").addClass("show")
+})
+$(".square6").click(function(){
+    $(".x6").addClass("show")
+})
+$(".square7").click(function(){
+    $(".x7").addClass("show")
+})
+$(".square8").click(function(){
+    $(".x8").addClass("show")
+})
+$(".square9").click(function(){
+    $(".x9").addClass("show")
+})

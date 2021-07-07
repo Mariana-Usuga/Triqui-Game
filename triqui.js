16886d103f108cf2f125ac1ba64afa320644677c
Mariana Usuga <marianausuga275004@outlook.es>
2021-07-07 15:09:08 -0500

    add possible plays with javascript

--- triqui.js
+++ triqui.js
@@ -1,55 +1,137 @@
 
-var square=["x1","x2","x3","x4","x5","x6","x7","x8","x9"]
+
+
+var square=[["x1","x2","x3"],["x4","x5","x6"],["x7","x8","x9"]];
 var started=false;
 
 $(".square").click(function(){
-    var choosePlayer1=$(this).attr("name")
+    var choosePlayer1=$(this).attr("id")
     $("."+choosePlayer1).addClass("show")
 
-    setTimeout(function(){
-        var numberRandom=Math.floor(Math.random()*9)
-        var choosePlayer2=square[numberRandom]
-        while(choosePlayer2===choosePlayer1){
-        numberRandom=Math.floor(Math.random()*9)
-        choosePlayer2=square[numberRandom]
+    for(q=0; q<square.length; q++){
+        for(w=0; w<square.length; w++){
+            if(square[q][w]===choosePlayer1){
+            square[q][w]="x"
+            break;
+        }
     }
-    alert(numberRandom)
-    $(".x"+numberRandom).addClass("show")
-        
-    },700);
-})
-$(".square4").click(function(){
-    $(".x4").addClass("show")
-    setTimeout(player2,700)
-})
-$(".square5").click(function(){
-    $(".x5").addClass("show")
-    setTimeout(player2,700)
-})
-$(".square6").click(function(){
-    $(".x6").addClass("show")
-    setTimeout(player2,700)
-})
-$(".square7").click(function(){
-    $(".x7").addClass("show")
-    setTimeout(player2,700)
-})
-$(".square8").click(function(){
-    $(".x8").addClass("show")
-    setTimeout(player2,700)
-})
-$(".square9").click(function(){
-    $(".x9").addClass("show")
-    setTimeout(player2,700)
-})
-var player2=function(){
-    var numberRandom=Math.floor(Math.random()*9)
-    var choosePlayer2=square[numberRandom]
-    while(choosePlayer2===choosePlayer1){
-    numberRandom=Math.floor(Math.random()*9)
-    choosePlayer2=square[numberRandom]
 }
-alert(numberRandom)
-$(".x"+numberRandom).addClass("show")
-    
+    if(started===false){
+        first(square)
+        started=true;
+    }
+    else{
+        player2(square)
 }
+
+} )
+
+function first(square){
+    var randomNumber = Math.floor(Math.random() * 3);
+    var randomNumber2 = Math.floor(Math.random() * 3);
+    var randomChoose=square[randomNumber][randomNumber2];
+    while(randomChoose==="x"){
+    randomNumber = Math.floor(Math.random() * 3);
+    randomNumber2 = Math.floor(Math.random() * 3);
+    randomChoose=square[randomNumber][randomNumber2];
+    }
+    setTimeout(function(){
+        $("."+randomChoose)[0].innerHTML="O"
+        $("."+randomChoose).addClass("show")
+                randomChoose="o"
+                console.log(square)
+    }, 3000)
+}
+
+function player2(square){
+    var ganaste=false;
+        if(square[0][0]==="x" && square[0][1]==="x" && square[0][2]==="x"){
+            alert("ganaste")
+            ganaste=true;
+        }
+        else if(square[1][0]==="x" && square[1][1]==="x" && square[1][2]==="x"){
+            console.log("ganaste")
+            ganaste=true;
+        }
+        else if(square[2][0]==="x" && square[2][1]==="x" && square[2][2]==="x"){
+            console.log("ganaste")
+            ganaste=true;
+        }
+        /*----columnas----*/
+        else if(square[0][0]==="x" && square[1][0]==="x" && square[2][0]==="x"){
+            console.log("ganaste")
+            ganaste=true;
+        }
+        else if(square[0][1]==="x" && square[1][1]==="x" && square[2][1]==="x"){
+            console.log("ganaste")
+            ganaste=true;
+        }
+        else if(square[0][2]==="x" && square[1][2]==="x" && square[2][2]==="x"){
+            console.log("ganaste")
+            ganaste=true;
+        }
+        else if(square[0][0]==="x" && square[1][1]==="x" && square[2][2]==="x"){
+            console.log("ganaste")
+            ganaste=true;
+        }
+        else if(square[0][2]==="x" && square[1][1]==="x" && square[2][0]==="x"){
+            console.log("ganaste")
+            ganaste=true;
+        }
+        else{
+            /*--------Filas---------*/
+           /* if(ganaste===false){
+            alert("workin")
+            for(i=0; i<square.lenght; i++){
+            
+            if(square[i][0]!=="x" && square[i][1]==="x" && square[i][2]==="x"){
+                    alert("workinggggg")
+                    $("."+square[i][0])[0].innerHTML="O"
+                    $("."+square[i][0]).addClass("show")
+                    square[i][0]="o";    
+                break;
+            }
+            else if(square[i][0]==="x" && square[i][1]!=="x" && square[i][2]==="x"){
+                /*setTimeout(function(){
+                alert("workingggg")
+                $("."+square[i][1])[0].innerHTML="O"
+                $("."+square[i][1]).addClass("show")
+                square[i][1]="o"
+                break;
+            }*/
+            if(square[2][0]==="x" && square[2][1]==="x" && square[2][2]!=="x"){
+                /*setTimeout(function(){*/
+                alert("workingggg")
+                $("."+square[2][2])[0].innerHTML="O"
+                $("."+square[2][2]).addClass("show")
+                square[2][2]="o"
+                
+            }
+    }
+}
+
+    /*
+    else{
+        -------------Columnas---------
+        for(k=0; k<square.lenght; k++){
+            var l=0;
+
+        if(square[l][k]!=="x" && square[l+1][k]==="x" && square[l+2][k]==="x"){
+            $("."+square[l][k])[0].innerHTML="O"
+            $("."+square[l][k]).addClass("show")
+            square[l][k]="o"
+        }
+        else if(square[l][k]==="x" && square[l+1][k]!=="x" && square[l+2][k]==="x"){
+            $("."+square[l+1][k])[0].innerHTML="O"
+            $("."+square[l+1][k]).addClass("show")
+            square[l+1][k]="o"
+        }
+        else if(square[l][k]==="x" && square[l+1][k]==="x" && square[l+2][k]!=="x"){
+            $("."+square[l+2][k])[0].innerHTML="O"
+            $("."+square[l+2][k]).addClass("show")
+            square[l+2][k]="o"
+        }
+    }
+    }
+    }
+    }*/
